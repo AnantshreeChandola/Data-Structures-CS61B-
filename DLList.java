61af20d28bf87deef53df4dcb00a7a3193319fdf
Anantshree Chandola <37929460+AnantshreeChandola@users.noreply.github.com>
2021-07-06 21:10:13 +0530

    Generic Doubly Linked List Implementation with Sentinel
    Related Concept: Obscurantism in Java
    Complexity-  
    Access: O(N)	
    Search: O(N)	
    Insert: O(1)	
    Delete: O(1)

---
+++ DLList.java
@@ -0,0 +1,72 @@
+public class DLList<bloop>{
+	private Node sentinel;
+	private int size;
+
+	private class Node{
+		public bloop item;
+		public Node next;
+		public Node prev;
+
+		private Node(bloop x, Node nodenext, Node nodeprev){
+			item = x;
+			next = nodenext;
+			prev = nodeprev;
+		}
+	}
+	
+	public DLList(){
+		sentinel = new Node(null, null, null);
+		sentinel.next = sentinel;
+		sentinel.prev = sentinel; 
+		size = 0; 
+	}
+	public void addfirst(bloop x){
+		Node y = new Node(x, sentinel.next, sentinel);
+		sentinel.next = y;
+		y.next.prev = y;
+		size += 1;	
+	}
+	public bloop getfirst(){
+		return sentinel.next.item;
+	}
+	public bloop getlast(){
+		return sentinel.prev.item;
+	}
+	public void addlast(bloop x){
+		Node y = new Node(x, null, sentinel.prev);
+		Node temp = sentinel.prev;
+		sentinel.prev = y;
+		y.prev = temp;
+		y.next = y;
+		size += 1;
+	}
+	public void removefirst(){
+		sentinel.next = sentinel.next.next;
+		sentinel.next.prev = sentinel;
+		size -= 1;
+	}
+	public void removelast(){
+		sentinel.prev = sentinel.prev.prev;
+		sentinel.prev.next = null;
+		size -= 1;
+	}
+	public int getsize(){
+		return size;
+	}
+	public static void main(String[] args){
+		DLList<Integer> s = new DLList<Integer>();
+		s.addfirst(1);
+		s.addfirst(2);
+		s.addfirst(3);
+		s.addlast(4);
+		s.addlast(5);
+		System.out.println(s.getfirst());
+		System.out.println(s.getsize());
+		s.removefirst();
+		System.out.println(s.getfirst());
+		System.out.println(s.getsize());
+		s.removelast();
+		System.out.println(s.getlast());
+		System.out.println(s.getsize());
+	}
+}
