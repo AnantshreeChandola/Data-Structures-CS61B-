39af85e9b778d4528043bb8ad3ec26f235f6c60f
Anantshree Chandola <37929460+AnantshreeChandola@users.noreply.github.com>
2021-08-01 02:32:02 +0530

    Shortest path between s(source) and t(destination).
    Time Complexity: O(V+E)

--- Graph.py
+++ Graph.py
@@ -6,29 +6,14 @@ class Graph:
     def Addedge(self, s, t):
         self.graph[s].append(t)
 
-    def Dfputil(self, s, marked, edgeto):
-        marked.add(s)
-        for i in self.graph[s]:
-            if i not in marked:
-                edgeto.append((s, i))
-                self.Dfputil(i, marked, edgeto)
-        return edgeto
-    def Depthfirstpath(self, s):
-        marked = set()
-        edgeto = []
-        print(self.Dfputil(s, marked, edgeto))
-
     def ShortestdistanceUtil(self, s, t, dist, pre):
         queue = [s]
-        marked = set()
-        marked.add(s)
         dist[s] = 0
         pre[s] = None
         while queue:
             previous = queue.pop(0)
             for next in self.graph[previous]:
-                if next not in marked:
-                    marked.add(next)
+                if (dist[next] == 0 or dist[next]>dist[previous]+1) and next!=s:
                     queue.append(next)
                     pre[next] = previous
                     dist[next] = dist[previous]+1
@@ -36,24 +21,24 @@ class Graph:
                         return True
         return False
     def PrintShortestPath(self, s, t):
-        dist = {}
-        pre = {}
+        dist = defaultdict(int)
+        pre = defaultdict(str)
         path = [t]
-        self.ShortestdistanceUtil(s, t, dist, pre)
-        while pre[t] != None:
-            path.append(pre[t])
-            t = pre[t]
-        print(path[::-1])
+        if not self.ShortestdistanceUtil(s, t, dist, pre):
+            print(False)
+        else:
+            while pre[t] != None:
+                path.append(pre[t])
+                t = pre[t]
+            print(path[::-1])
 
 
 g = Graph()
 g.Addedge('A', 'B')
 g.Addedge('A', 'C')
-g.Addedge('B', 'C')
+g.Addedge('B', 'E')
+g.Addedge('E', 'D')
+g.Addedge('E', 'D')
 g.Addedge('C', 'D')
-g.Addedge('D', 'A')
-g.Addedge('D', 'B')
-g.Addedge('B', 'A')
-g.Depthfirstpath('A')
 g.PrintShortestPath('A', 'D')
 
